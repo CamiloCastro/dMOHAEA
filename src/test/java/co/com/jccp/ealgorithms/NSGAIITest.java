9926995d90e81557055ad60c0352e6792cc5de82
Juan Camilo Castro Pinto <jccastrop@unal.edu.co>
2019-09-10 09:49:37 -0500

    Add new test functions and images.

--- src/test/java/co/com/jccp/ealgorithms/NSGAIITest.java
+++ src/test/java/co/com/jccp/ealgorithms/NSGAIITest.java
@@ -38,12 +38,12 @@ public class NSGAIITest {
 //        double[] xlim = new double[]{0, 1};
 //        double[] ylim = new double[]{0, 1};
 
-        POL function = new POL();
-        int dimensions = 2;
-        double[][] limits =  new double[][]{{-Math.PI, Math.PI}, {-Math.PI, Math.PI}};
-        String fileName = "NSGAII-POL";
-        double[] xlim = new double[]{1, 17};
-        double[] ylim = new double[]{0, 29};
+//        POL function = new POL();
+//        int dimensions = 2;
+//        double[][] limits =  new double[][]{{-Math.PI, Math.PI}, {-Math.PI, Math.PI}};
+//        String fileName = "NSGAII-POL";
+//        double[] xlim = new double[]{1, 17};
+//        double[] ylim = new double[]{0, 29};
 
 //        KUR function = new KUR();
 //        int dimensions = 3;
@@ -75,67 +75,147 @@ public class NSGAIITest {
 //        String fileName = "NSGAII-ZDT2";
 //        double[] xlim = new double[]{0, 1};
 //        double[] ylim = new double[]{0, 1};
+//
+//
+//        ZDT3 function = new ZDT3();
+//        int dimensions = 30;
+//        double[][] limits = new double[dimensions][2];
+//
+//        for (int i = 0; i < dimensions; i++) {
+//            limits[i][0] = 0.0;
+//            limits[i][1] = 1.0;
+//        }
+//        String fileName = "NSGAII-ZDT3";
+//        double[] xlim = new double[]{0, 1};
+//        double[] ylim = new double[]{0, 1};
+
+//        ZDT4 function = new ZDT4();
+//        int dimensions = 10;
+//        double[][] limits = new double[dimensions][2];
+//
+//        limits[0][0] = 0.0;
+//        limits[0][1] = 1.0;
+//
+//        for (int i = 1; i < dimensions; i++) {
+//            limits[i][0] = -5.0;
+//            limits[i][1] = 5.0;
+//        }
+//        String fileName = "NSGAII-ZDT4";
+//        double[] xlim = new double[]{0, 1};
+//        double[] ylim = new double[]{0, 2};
+
+        ZDT6 function = new ZDT6();
+        int dimensions = 10;
+        double[][] limits = new double[dimensions][2];
+
+        for (int i = 0; i < dimensions; i++) {
+            limits[i][0] = 0.0;
+            limits[i][1] = 1.0;
+        }
+        String fileName = "NSGAII-ZDT6";
+        double[] xlim = new double[]{0, 1};
+        double[] ylim = new double[]{0, 2};
+
+        double converg[] = new double[10];
+        double diversity[] = new double[10];
+
+        for (int ii = 0; ii < 10; ii++) {
+            RandomRealInitialization init = new RandomRealInitialization(limits);
+
+            boolean minimize = true;
+
+            int popSize = 100;
+
+            double mutProb = 1;
+
+            double xoverProb = 0.9;
+
+            int iterations = 250;
+
+            SimulatedBinaryXover sbx = new SimulatedBinaryXover(20.0, limits);
+
+            PolynomialMutation pm = new PolynomialMutation(20.0, limits);
+
+            RealCloneUtil rcu = new RealCloneUtil();
+
+            NSGAII<double[]> nsga = new NSGAII<>(init, function, minimize, dimensions, popSize, mutProb, xoverProb, iterations, sbx, pm, rcu);
+
+            List<MOEAIndividual<double[]>> answer = nsga.apply();
+
+            double[] x = new double[answer.size()];
+            double[] y = new double[answer.size()];
 
 
 
-        RandomRealInitialization init = new RandomRealInitialization(limits);
+            for (int i = 0; i < answer.size(); i++) {
+                x[i] = answer.get(i).getObjectiveValues()[0];
+                y[i] = answer.get(i).getObjectiveValues()[1];
+            }
 
-        boolean minimize = true;
+            double[][] opt = function.optimal(500);
 
-        int popSize = 100;
+            double[] xopt = new double[opt.length];
+            double[] yopt = new double[opt.length];
 
-        double mutProb = 1;
+            for (int i = 0; i < opt.length; i++) {
+                xopt[i] = opt[i][0];
+                yopt[i] = opt[i][1];
+            }
 
-        double xoverProb = 0.9;
+            converg[ii] = Convergence.calculate(new double[][]{x, y}, new double[][]{xopt, yopt});
+            diversity[ii] = Diversity.calculate(new double[][]{x, y}, new double[][]{xopt, yopt});
 
-        int iterations = 300;
+//            System.out.println(Convergence.calculate(new double[][]{x, y}, new double[][]{xopt, yopt}));
+//            System.out.println(Diversity.calculate(new double[][]{x, y}, new double[][]{xopt, yopt}));
 
-        SimulatedBinaryXover sbx = new SimulatedBinaryXover(20.0, limits);
-
-        PolynomialMutation pm = new PolynomialMutation(20.0, limits);
-
-        RealCloneUtil rcu = new RealCloneUtil();
-
-        NSGAII<double[]> nsga = new NSGAII<>(init, function, minimize, dimensions, popSize, mutProb, xoverProb, iterations, sbx, pm, rcu);
-
-        List<MOEAIndividual<double[]>> answer = nsga.apply();
-
-        double[] x = new double[answer.size()];
-        double[] y = new double[answer.size()];
-
-
-
-        for (int i = 0; i < answer.size(); i++) {
-            x[i] = answer.get(i).getObjectiveValues()[0];
-            y[i] = answer.get(i).getObjectiveValues()[1];
+            MatlabChart fig = new MatlabChart(); // figure('Position',[100 100 640 480]);
+            fig.plot(x, y, ".r", 2.0f, "Found"); // plot(x,y1,'-r','LineWidth',2);
+            fig.plot(xopt, yopt, "-b", 1.0f, "Optimal"); // plot(x,y1,'-r','LineWidth',2);
+            fig.RenderPlot();                    // First render plot before modifying
+            fig.title(fileName);    // title('Stock 1 vs. Stock 2');
+            fig.xlim(xlim[0], xlim[1]);                   // xlim([10 100]);
+            fig.ylim(ylim[0], ylim[1]);                  // ylim([200 300]);
+            fig.xlabel("Objective 1");                  // xlabel('Days');
+            fig.ylabel("Objective 2");                 // ylabel('Price');
+            fig.grid("on","on");                 // grid on;
+            fig.legend("northeast");             // legend('AAPL','BAC','Location','northeast')
+            fig.font("Helvetica",15);            // .. 'FontName','Helvetica','FontSize',15
+            fig.saveasSVG("NSGAII/" + fileName + "-" + ii + ".svg",640,640);   // saveas(gcf,'MyPlot','jpeg');
         }
 
-        double[][] opt = function.optimal(500);
+        double meanConv = 0.0;
+        double meanDivers = 0.0;
+        for (int i = 0; i < 10; i++) {
+            meanConv += converg[i];
+            meanDivers += diversity[i];
+        }
+        meanConv /= 10.0;
+        meanDivers /= 10.0;
 
-        double[] xopt = new double[opt.length];
-        double[] yopt = new double[opt.length];
+        double varConv = 0.0;
+        double varDivers = 0.0;
 
-        for (int i = 0; i < opt.length; i++) {
-            xopt[i] = opt[i][0];
-            yopt[i] = opt[i][1];
+        for (int i = 0; i < 10; i++) {
+            varConv += (converg[i] - meanConv) * (converg[i] - meanConv);
+            varDivers += (diversity[i] - meanDivers) * (diversity[i] - meanDivers);
         }
 
-        System.out.println(Convergence.calculate(new double[][]{x, y}, new double[][]{xopt, yopt}));
-        System.out.println(Diversity.calculate(new double[][]{x, y}, new double[][]{xopt, yopt}));
+        varConv /= 9.0;
+        varDivers /= 9.0;
 
-        MatlabChart fig = new MatlabChart(); // figure('Position',[100 100 640 480]);
-        fig.plot(x, y, ".r", 2.0f, "Found"); // plot(x,y1,'-r','LineWidth',2);
-        fig.plot(xopt, yopt, "-b", 1.0f, "Optimal"); // plot(x,y1,'-r','LineWidth',2);
-        fig.RenderPlot();                    // First render plot before modifying
-        fig.title(fileName);    // title('Stock 1 vs. Stock 2');
-        fig.xlim(xlim[0], xlim[1]);                   // xlim([10 100]);
-        fig.ylim(ylim[0], ylim[1]);                  // ylim([200 300]);
-        fig.xlabel("Objective 1");                  // xlabel('Days');
-        fig.ylabel("Objective 2");                 // ylabel('Price');
-        fig.grid("on","on");                 // grid on;
-        fig.legend("northeast");             // legend('AAPL','BAC','Location','northeast')
-        fig.font("Helvetica",15);            // .. 'FontName','Helvetica','FontSize',15
-        fig.saveasSVG("NSGAII/" + fileName + ".svg",640,640);   // saveas(gcf,'MyPlot','jpeg');
+        System.out.println("MEAN CONVERGENCE: " + meanConv);
+        System.out.println("DEVIATION CONVERGENCE: " + varConv);
+        System.out.println();
+        System.out.println("MEAN DIVERSITY: " + meanDivers);
+        System.out.println("DEVIATION DIVERSITY: " + varDivers);
+
+
+
+
+
+
+
+
 
 
 
--- src/test/java/co/com/jccp/ealgorithms/SPEA2Test.java
+++ src/test/java/co/com/jccp/ealgorithms/SPEA2Test.java
@@ -20,8 +20,7 @@ import java.util.List;
 public class SPEA2Test {
 
     @Test
-    public void spea2Test()
-    {
+    public void spea2Test() {
 
 //        SCH function = new SCH();
 //        int dimensions = 1;
@@ -48,80 +47,153 @@ public class SPEA2Test {
 //        String fileName = "SPEA2-ZDT1";
 //        double[] xlim = new double[]{0, 1};
 //        double[] ylim = new double[]{0, 1};
+//
+//        ZDT2 function = new ZDT2();
+//        int dimensions = 30;
+//        double[][] limits = new double[dimensions][2];
+//
+//        for (int i = 0; i < dimensions; i++) {
+//            limits[i][0] = 0.0;
+//            limits[i][1] = 1.0;
+//        }
+//        String fileName = "SPEA2-ZDT2";
+//        double[] xlim = new double[]{0, 1};
+//        double[] ylim = new double[]{0, 1};
+//
+//        ZDT3 function = new ZDT3();
+//        int dimensions = 30;
+//        double[][] limits = new double[dimensions][2];
+//
+//        for (int i = 0; i < dimensions; i++) {
+//            limits[i][0] = 0.0;
+//            limits[i][1] = 1.0;
+//        }
+//        String fileName = "SPEA2-ZDT3";
+//        double[] xlim = new double[]{0, 1};
+//        double[] ylim = new double[]{0, 1};
+//
+//        double converg[] = new double[10];
+//        double diversity[] = new double[10];
 
-        ZDT2 function = new ZDT2();
-        int dimensions = 30;
+//        ZDT4 function = new ZDT4();
+//        int dimensions = 10;
+//        double[][] limits = new double[dimensions][2];
+//
+//        limits[0][0] = 0.0;
+//        limits[0][1] = 1.0;
+//
+//        for (int i = 1; i < dimensions; i++) {
+//            limits[i][0] = -5.0;
+//            limits[i][1] = 5.0;
+//        }
+//        String fileName = "SPEA2-ZDT4";
+//        double[] xlim = new double[]{0, 1};
+//        double[] ylim = new double[]{0, 2};
+
+        ZDT6 function = new ZDT6();
+        int dimensions = 10;
         double[][] limits = new double[dimensions][2];
 
         for (int i = 0; i < dimensions; i++) {
             limits[i][0] = 0.0;
             limits[i][1] = 1.0;
         }
-        String fileName = "SPEA2-ZDT2";
+        String fileName = "SPEA2-ZDT6";
         double[] xlim = new double[]{0, 1};
-        double[] ylim = new double[]{0, 1};
+        double[] ylim = new double[]{0, 2};
 
 
+        double[] converg = new double[10];
+        double[] diversity = new double[10];
 
-        RandomRealInitialization init = new RandomRealInitialization(limits);
+        for (int ii = 0; ii < 10; ii++) {
 
-        boolean minimize = true;
+            RandomRealInitialization init = new RandomRealInitialization(limits);
 
-        int popSize = 300;
+            boolean minimize = true;
 
-        double mutProb = 1;
+            int popSize = 100;
 
-        double xoverProb = 0.9;
+            double mutProb = 1;
 
-        int iterations = 250;
+            double xoverProb = 0.9;
 
-        SimulatedBinaryXover sbx = new SimulatedBinaryXover(20.0, limits);
+            int iterations = 250;
 
-        PolynomialMutation pm = new PolynomialMutation(20.0, limits);
+            SimulatedBinaryXover sbx = new SimulatedBinaryXover(20.0, limits);
 
-        RealCloneUtil rcu = new RealCloneUtil();
-        
-        int archiveSize = 100;
+            PolynomialMutation pm = new PolynomialMutation(20.0, limits);
 
-        SPEA2<double[]> spea2 = new SPEA2<>(init, function, minimize, dimensions, popSize, mutProb, xoverProb, iterations, sbx, pm, rcu, archiveSize);
+            RealCloneUtil rcu = new RealCloneUtil();
 
-        List<MOEAIndividual<double[]>> answer = spea2.apply();
+            int archiveSize = 100;
 
-        double[] x = new double[answer.size()];
-        double[] y = new double[answer.size()];
+            SPEA2<double[]> spea2 = new SPEA2<>(init, function, minimize, dimensions, popSize, mutProb, xoverProb, iterations, sbx, pm, rcu, archiveSize);
 
-        double[] xopt = new double[500];
-        double[] yopt = new double[500];
+            List<MOEAIndividual<double[]>> answer = spea2.apply();
 
-        for (int i = 0; i < answer.size(); i++) {
-            x[i] = answer.get(i).getObjectiveValues()[0];
-            y[i] = answer.get(i).getObjectiveValues()[1];
+            double[] x = new double[answer.size()];
+            double[] y = new double[answer.size()];
+
+            double[] xopt = new double[500];
+            double[] yopt = new double[500];
+
+            for (int i = 0; i < answer.size(); i++) {
+                x[i] = answer.get(i).getObjectiveValues()[0];
+                y[i] = answer.get(i).getObjectiveValues()[1];
+            }
+
+            double[][] opt = function.optimal(500);
+
+            for (int i = 0; i < 500; i++) {
+                xopt[i] = opt[i][0];
+                yopt[i] = opt[i][1];
+            }
+
+            converg[ii] = Convergence.calculate(new double[][]{x, y}, new double[][]{xopt, yopt});
+            diversity[ii] = Diversity.calculate(new double[][]{x, y}, new double[][]{xopt, yopt});
+
+            MatlabChart fig = new MatlabChart(); // figure('Position',[100 100 640 480]);
+            fig.plot(x, y, ".r", 2.0f, "Found"); // plot(x,y1,'-r','LineWidth',2);
+            fig.plot(xopt, yopt, "-b", 1.0f, "Optimal"); // plot(x,y1,'-r','LineWidth',2);
+            fig.RenderPlot();                    // First render plot before modifying
+            fig.title(fileName);    // title('Stock 1 vs. Stock 2');
+            fig.xlim(xlim[0], xlim[1]);                   // xlim([10 100]);
+            fig.ylim(ylim[0], ylim[1]);                  // ylim([200 300]);
+            fig.xlabel("Objective 1");                  // xlabel('Days');
+            fig.ylabel("Objective 2");                 // ylabel('Price');
+            fig.grid("on", "on");                 // grid on;
+            fig.legend("northeast");             // legend('AAPL','BAC','Location','northeast')
+            fig.font("Helvetica", 15);            // .. 'FontName','Helvetica','FontSize',15
+            fig.saveasSVG("SPEA2/" + fileName + "-" + ii + ".svg", 640, 640);   // saveas(gcf,'MyPlot','jpeg');
+
         }
 
-        double[][] opt = function.optimal(500);
+        double meanConv = 0.0;
+        double meanDivers = 0.0;
+        for (int i = 0; i < 10; i++) {
+            meanConv += converg[i];
+            meanDivers += diversity[i];
+        }
+        meanConv /= 10.0;
+        meanDivers /= 10.0;
 
-        for (int i = 0; i < 500; i++) {
-            xopt[i] = opt[i][0];
-            yopt[i] = opt[i][1];
+        double varConv = 0.0;
+        double varDivers = 0.0;
+
+        for (int i = 0; i < 10; i++) {
+            varConv += (converg[i] - meanConv) * (converg[i] - meanConv);
+            varDivers += (diversity[i] - meanDivers) * (diversity[i] - meanDivers);
         }
 
-        System.out.println(Convergence.calculate(new double[][]{x, y}, new double[][]{xopt, yopt}));
-        System.out.println(Diversity.calculate(new double[][]{x, y}, new double[][]{xopt, yopt}));
+        varConv /= 9.0;
+        varDivers /= 9.0;
 
-        MatlabChart fig = new MatlabChart(); // figure('Position',[100 100 640 480]);
-        fig.plot(x, y, ".r", 2.0f, "Found"); // plot(x,y1,'-r','LineWidth',2);
-        fig.plot(xopt, yopt, "-b", 1.0f, "Optimal"); // plot(x,y1,'-r','LineWidth',2);
-        fig.RenderPlot();                    // First render plot before modifying
-        fig.title(fileName);    // title('Stock 1 vs. Stock 2');
-        fig.xlim(xlim[0], xlim[1]);                   // xlim([10 100]);
-        fig.ylim(ylim[0], ylim[1]);                  // ylim([200 300]);
-        fig.xlabel("Objective 1");                  // xlabel('Days');
-        fig.ylabel("Objective 2");                 // ylabel('Price');
-        fig.grid("on","on");                 // grid on;
-        fig.legend("northeast");             // legend('AAPL','BAC','Location','northeast')
-        fig.font("Helvetica",15);            // .. 'FontName','Helvetica','FontSize',15
-        fig.saveasSVG("SPEA2/" + fileName + ".svg",640,640);   // saveas(gcf,'MyPlot','jpeg');
-
+        System.out.println("MEAN CONVERGENCE: " + meanConv);
+        System.out.println("DEVIATION CONVERGENCE: " + varConv);
+        System.out.println();
+        System.out.println("MEAN DIVERSITY: " + meanDivers);
+        System.out.println("DEVIATION DIVERSITY: " + varDivers);
 
 
     }
